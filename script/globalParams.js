8bd1590d126e8eda8db94ff06873cbf9f4760b24
mirko <mirko@dell-desktop.(none)>
2013-04-06 14:29:31 +0200

    Add bbox configuration (manual setting)

--- script/globalParams.js
+++ script/globalParams.js
@@ -1,14 +1,34 @@
-// Parametro server IP/Indirizzo
-var server = "192.168.1.93";
-// Percorso server project
-var pathProject = "/var/www/jq-webclient/projects";
-// Nome Progetto
-var nameProject = "polygon.qgs";
+/* 
+ * *******************
+ * SETTINGS PARAMETERS
+ * *******************
+ */
+
+/*
+ * IP or server url of machine where jq-weclient is installed
+ * Examples: 
+ * 	- 192.168.1.12
+ * 	- test.yourserver.com
+ */
+var server = "";
+
+/*
+ * Path where you copy your qgis project (qgs file)
+ * I suggest to copy your qgs file into "project" directory of jq-webclient
+ * Example: /var/www/jq-webclient/projects
+ */
+var pathProject = "";
+
+/*
+ * File name of your qgis project (example: polygon.qgs)
+ */
+var nameProject = "";
 
 /* EPSG
  * This is the epsg code for the coordinates you want to display on map
-*/
-var epsgcode_display = "3003"; //
+ * Leave empty if you want to display coordinates in the same epsg of project 
+ */
+var epsgcode_display = ""; //
 
 /* BBOX
  * If your qgis project has a bbox configured you can leave these parameters empty.
@@ -19,23 +39,46 @@ var bbox_miny = ""; // Y min
 var bbox_maxx = ""; // X max
 var bbox_maxy = ""; // Y max
 
-// Unit (degree, m ..)
+/*
+ * Unit of coordinate map
+ * m : meters
+ * degree: degree
+ */
 var unit = "m";
-// Scale
-var maxScale = "200"; // Massima
-var minScale = "50000"; // Minima
-
-// LOAD GOOGLE
-// ATTENTION: to enable this option keep in mind that qgs project must have epsg 900913 or equivalent (3857 for example) 
-var GOOGLE_ENABLE = true;
-// LOAD OSM
-var OSM_ENABLE = false;
-
-var CACHE_BROWSER = true;
-
 
 /*
- * NOT MODIFY THE FOLLOWING 
+ * Scale
+ * You can set here max and min scale of map
+ */ 
+var maxScale = "200"; // Max
+var minScale = "50000"; // Min
+
+/*
+ * LOAD GOOGLE 
+ * true to enable google layer
+ * 
+ * ATTENTION: to enable this option keep in mind that qgs project must have epsg 900913 or equivalent (3857 for example) 
+ */
+var GOOGLE_ENABLE = false;
+
+/* 
+ * LOAD OSM
+ * true to enable Open Street Map layer 
+ */
+var OSM_ENABLE = false;
+
+/*
+ * true to enable local storage of browser
+ * Browser will cache during moving and zooming map
+ */
+var CACHE_BROWSER = false;
+
+
+
+/* 
+ * ************************
+ * NOT MODIFY THE FOLLOWING
+ * ************************ 
  */
 // WMS URI
 var serverURI = "http://"+server+"/cgi-bin/qgis_mapserv.fcgi?map="+pathProject+"/"+nameProject;
